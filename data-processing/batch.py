168689410bfb57240e8217c50e6caeb6f998a894
shao-shuai <shuai-shao@outlook.com>
2020-02-14 16:04:28 -0800

    add function name for writing company name

--- data-processing/batch.py
+++ data-processing/batch.py
@@ -1,8 +1,6 @@
 import sys
-import boto3
-import pandas as pd
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import udf
+from pyspark.sql.functions import udf, col
 from pyspark.sql.types import StringType
 from pyspark.sql.types import IntegerType
 from pyspark.sql.functions import desc
@@ -14,10 +12,6 @@ import pyspark.sql.functions as sf
 
 import datetime
 
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-
 from neo4j import GraphDatabase
 import neo4j
 
@@ -26,7 +20,7 @@ from functools import reduce
 from pyspark.sql import DataFrame
 import os
 
-logging.basicConfig(filename = 'neo4j-4.log', level = logging.INFO,
+logging.basicConfig(filename = 'write_company.log', level = logging.INFO,
                     format = '%(asctime)s:%(levelname)s:%(message)s')
 
 spark = SparkSession\
@@ -46,21 +40,23 @@ def get_publication_number(path):
 
 def get_publication_number_company(path):
     """Get publication nubmer and company name of patent."""
+    join_udf = udf(lambda x: ",".join(x))
+
     patent = spark.read.json(path)
     publication_number_company = patent \
-        .select(['publication_number', explode('assignee_harmonized.name')])
-
+        .select(['publication_number', 'assignee_harmonized.name']) \
+        .withColumn('name', join_udf(col("name")))
     return publication_number_company
 
 def file_list(index_start, index_end):
-    """Construct file list for reading."""
+    """Construct a list of file path on S3."""
     base_path = os.environ['S3ADDRESS']
     data_path = [base_path + '{0:012}'.format(i) + '.json' for i in range(index_start, index_end)]
 
     return data_path
 
 def single_file(file_index):
-    """Return the address of a single file."""
+    """Return the path of a single file."""
     base_path = os.environ['S3ADDRESS']
     data_path = base_path + '{0:012}'.format(file_index) + '.json'
 
@@ -77,12 +73,14 @@ def get_backward_citation(path):
     return backward_citation
 
 def write_neo4j_constraint():
+    '''Register index for label PATENT.name.'''
     query='''CREATE CONSTRAINT ON (p:PATENT) ASSERT p.name IS UNIQUE'''
     with gradb.session() as session:
         with session.begin_transaction() as tx:
             tx.run(query)
 
 def write_neo4j_relationship(data):
+    '''Neo4j session for writing relationship.'''
 
     query='''WITH $names AS nested
     UNWIND nested AS x
@@ -94,14 +92,36 @@ def write_neo4j_relationship(data):
         with session.begin_transaction() as tx:
             tx.run(query, names=data)
 
-#    MERGE (w)-[r:CITE]-(n)
-
 def write_database_relationship(path):
+    '''Function for writing relationship.'''
     data = get_backward_citation(i)
     data = data.collect()
     relationship = [[i['pnum'], i['col']] for i in data]
     write_neo4j_relationship(relationship)
     relationship = []
+
+def write_property_session(data):
+    '''Writing property company name for each node.'''
+
+    query = '''WITH $names AS nested
+    UNWIND nested AS x
+    MATCH (n:PATENT {name: x[0]})
+    SET n.company=x[1]
+    '''
+
+    with gradb.session() as session:
+        with session.begin_transaction() as tx:
+            tx.run(query, names=data)
+
+def write_database_property_company(path):
+    '''Function for writing company name.'''
+
+    data = get_publication_number_company(path)
+    data = data.collect()
+    relationship = [[i['publication_number'], i['name']] for i in data]
+    write_property_session(relationship)
+    relationship = []
+
 
 if __name__ == '__main__':
 
@@ -109,13 +129,21 @@ if __name__ == '__main__':
 
     data_path = file_list(int(sys.argv[1]), int(sys.argv[2]))
 
-#    write_neo4j_constraint()
+    for i in data_path:
+        '''Loop for writing relationship.'''
+
+        try:
+            write_database_property_company(i)
+            logging.info("Writing company " + i + " succeeded")
+        except Exception:
+             logging.info("Writing company " + i + " failed")
 
     for i in data_path:
+        '''Loop for writing company name as property of each node.'''
+
         try:
             write_database_relationship(i)
             logging.info("Writing file " + i + " succeeded")
         except Exception:
             logging.info("Writing file " + i + " failed")
-
 
